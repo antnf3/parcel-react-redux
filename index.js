ddad765ccf0f03fb17c7cfb2e84e0e7e1f8fc3f9
antnf3 <antnf3@gmail.com>
2020-03-30 08:40:23 +0900

    react-redux 연결

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./src/App";
+import { Provider } from "react-reduct";
+import store from "./store";
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
