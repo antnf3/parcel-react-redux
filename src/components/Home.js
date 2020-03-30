7504ddf5e65f650cd453d6f632048e73b9225d51
antnf3 <antnf3@gmail.com>
2020-03-30 09:10:27 +0900

    react-redux connect mapStateToProps 추가

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,20 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
+function Home({ toDos }) {
+  console.log(toDos);
   const [text, setText] = useState("");
   function onChange(e) {
-    // e.preventDefault();
     setText(e.target.value);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    setText("");
   }
   return (
     <Fragment>
       <h1>ToDo LIST</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <input type="text" value={text} onChange={onChange} />
         <button>ADD</button>
       </form>
@@ -18,4 +23,8 @@ function Home() {
   );
 }
 
-export default Home;
+function mapStateToProps(state, ownProps) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Home);
